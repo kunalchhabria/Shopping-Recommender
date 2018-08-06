97e8c4946a0d4fba4a31f259b042adfc67909e64
kunal chhabria <kunalchhabria525@gmail.com>
2018-08-07 00:01:01 +0530

    Add files via upload

---
+++ shop.py
@@ -0,0 +1,148 @@
+#-*- coding:utf-8 - *-
+
+import csv
+
+recommend={}
+
+def load_dataset():
+    "Load the sample dataset."
+    f=open('groceries.csv')
+    r=csv.reader(f)
+    data=list(r)
+    return data
+
+
+def createC1(dataset):
+    "Create a list of candidate item sets of size one."
+    c1 = []
+    for transaction in dataset:
+        for item in transaction:
+            if not [item] in c1:
+                c1.append([item])
+    c1.sort()
+    #frozenset because it will be a ket of a dictionary.
+    return map(frozenset, c1)
+
+
+def scanD(dataset, candidates, min_support):
+    "Returns all candidates that meets a minimum support level"
+    sscnt = {}
+    for tid in dataset:
+        for can in candidates:
+            if can.issubset(tid):
+                sscnt.setdefault(can, 0)
+                sscnt[can] += 1
+
+    num_items = float(len(dataset))
+    retlist = []
+    support_data = {}
+    for key in sscnt:
+        support = sscnt[key] / num_items
+        if support >= min_support:
+            retlist.insert(0, key)
+        support_data[key] = support
+    return retlist, support_data
+
+
+def shopGen(freq_sets, k):
+    "Generate the joint transactions from candidate sets"
+    retList = []
+    lenLk = len(freq_sets)
+    for i in range(lenLk):
+        for j in range(i + 1, lenLk):
+            L1 = list(freq_sets[i])[:k - 2]
+            L2 = list(freq_sets[j])[:k - 2]
+            L1.sort()
+            L2.sort()
+            if L1 == L2:
+                retList.append(freq_sets[i] | freq_sets[j])
+    return retList
+
+
+def shop(dataset, minsupport=0.5):
+    "Generate a list of candidate item sets"
+    C1 = createC1(dataset)
+    D = map(set, dataset)
+    L1, support_data = scanD(D, C1, minsupport)
+    L = [L1]
+    k = 2
+    while (len(L[k - 2]) > 0):
+        Ck = shopGen(L[k - 2], k)
+        Lk, supK = scanD(D, Ck, minsupport)
+        support_data.update(supK)
+        L.append(Lk)
+        k += 1
+
+    return L, support_data
+
+
+
+def generateRules(L, support_data, min_confidence=0.7):
+    """Create the association rules
+    L: list of frequent item sets
+    support_data: support data for those itemsets
+    min_confidence: minimum confidence threshold
+    """
+    rules = []
+    for i in range(1, len(L)):
+        for freqSet in L[i]:
+            H1 = [frozenset([item]) for item in freqSet]
+            #print "freqSet", freqSet, 'H1', H1
+            if (i > 1):
+                rules_from_conseq(freqSet, H1, support_data, rules, min_confidence)
+            else:
+                calc_confidence(freqSet, H1, support_data, rules, min_confidence)
+    return rules
+
+
+def calc_confidence(freqSet, H, support_data, rules, min_confidence=0.7):
+    "Evaluate the rule generated"
+    pruned_H = []
+    for conseq in H:
+        conf = support_data[freqSet] / support_data[freqSet - conseq]
+        if conf >= min_confidence:
+            print freqSet - conseq, '--->', conseq, 'conf:', conf , 'lift' , conf-min_confidence
+            for i in freqSet-conseq:
+                temp1=i
+                break
+            for j in conseq:
+                temp2=j
+                break
+            if not temp1 in recommend:
+            	recommend[temp1]=[]
+            
+            if not temp2 in recommend[temp1]:
+            	recommend[temp1].append(temp2)
+            
+            rules.append((freqSet - conseq, conseq, conf))
+            pruned_H.append(conseq)
+    return pruned_H
+
+
+def rules_from_conseq(freqSet, H, support_data, rules, min_confidence=0.7):
+    "Generate a set of candidate rules"
+    m = len(H[0])
+    if (len(freqSet) > (m + 1)):
+        Hmp1 = shopGen(H, m + 1)
+        Hmp1 = calc_confidence(freqSet, Hmp1,  support_data, rules, min_confidence)
+        if len(Hmp1) > 1:
+            rules_from_conseq(freqSet, Hmp1, support_data, rules, min_confidence)
+
+
+def startShopping():
+	item="a"
+	while(True):
+		print "Enter quit to finish shopping.\nWhat would you like to buy?"
+		item=raw_input()
+		if item=='quit':
+			break
+		if not item in recommend:
+			print "What else would you like to buy?"
+		else:
+			print "You might also like:"
+
+			for i in recommend[item]:
+				print i
+			
+		print "\n\n\n"
+	
